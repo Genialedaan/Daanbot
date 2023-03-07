41754d56392b8be0043cd3cf07f958f76fe86ba8
Daan Poppe <daanpoppe@hotmail.com>
2023-03-07 19:00:09 +0100

    Shuffle command added
    Command added for shuffling the current queue

---
+++ commands/shuffle.js
@@ -0,0 +1,23 @@
+const { SlashCommandBuilder } = require("@discordjs/builders")
+
+module.exports = {
+	data: new SlashCommandBuilder()
+        .setName("shuffle")
+        .setDescription("Shuffles the playlist"),
+	execute: async ({ client, interaction }) => {
+        // Get the queue for the server
+		const queue = client.player.nodes.get(interaction.guildId)
+
+        // Check if the queue is empty
+		if (!queue)
+		{
+			await interaction.reply("There are no songs in the queue")
+			return;
+		}
+
+        // Shuffle the current queue
+		queue.tracks.shuffle();
+
+        await interaction.reply("Queue has been shuffled.")
+	},
+}
